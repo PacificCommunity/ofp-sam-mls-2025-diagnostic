e542fa263522a1b73241f7d4ff1a98bbc4088f3f
Arni Magnusson <thisisarni@gmail.com>
2025-04-16 17:38:43 +1100

    Consolidate data.R script

--- TAF/data.R
+++ TAF/data.R
@@ -1,7 +1,7 @@
 # Prepare data, write CSV data tables
 
-# Before:
-# After:
+# Before: data_echo.ss_new (boot/data)
+# After:  catch.csv, cpue.csv, length_comps.csv, weight_comps.csv (data)
 
 library(TAF)
 library(r4ss)
@@ -10,43 +10,46 @@ library(magrittr)
 
 mkdir("data")
 
-# read in Stock Synthesis data file
-tmp_data = SS_readdat("boot/data/data_echo.ss_new")
-cpue <- tmp_data[["CPUE"]]
-colnames(cpue) <-c("year", "season", "fishery", "index", "cv")
-cpue_nocv=cpue[,-5]
+# Read in Stock Synthesis data file
+tmp_data <- SS_readdat("boot/data/data_echo.ss_new")
 
-write.taf(cpue_nocv, file = "data/cpue.csv")
-
-
-
-# extract length composition data; reformat from wide to long
-# only retain observations fitted in the model
-lencomp <- as.data.table(tmp_data$lencomp) %>%
-           .[,part:=NULL] %>%
-           .[,Nsamp:=NULL] %>%
-           melt(.,id.vars=c("year","month","fleet","sex")) %>%
-           .[,variable:=as.numeric(gsub("l","",variable))] %>%
-           .[year>0&value>0] %>%
-           setnames(.,c("fleet","variable","value"),c("fishery","length","freq")) %>%
-           .[order(fishery,year,month,length)]
-
-fwrite(lencomp,"data/lencomp.csv")
-
+# Extract catch
 catch <- tmp_data$catch
 names(catch) <- c("year", "season", "fishery", "catch", "cv")
-catch <- catch[catch$year > 1900,]
+catch <- catch[catch$year > 0,]
 write.taf(catch, dir="data")
 
-# extract length composition data; reformat from wide to long
-# only retain observations fitted in the model
-sizefreq <- as.data.table(tmp_data$sizefreq_data_list[[1]]) %>%
-           .[,part:=NULL] %>%
-           .[,Nsamp:=NULL] %>%
-           .[,method:=NULL] %>%
-           melt(.,id.vars=c("year","month","fleet","sex")) %>%
-           .[,variable:=as.numeric(gsub("a","",variable))] %>%
-           .[year>0&value>0] %>%
-           setnames(.,c("fleet","variable","value"),c("fishery","weight","freq")) %>%
-           .[order(fishery,year,month,length)]
-write.taf(sizefreq, "data/weight_comps.csv")
+# Extract CPUE
+cpue <- tmp_data$CPUE
+colnames(cpue) <- c("year", "season", "fishery", "index", "cv")
+
+# Extract length comps
+# reformat from wide to long, exclude negative years
+length.comps <- as.data.table(tmp_data$lencomp) %>%
+  .[, sex:=NULL] %>%
+  .[, part:=NULL] %>%
+  .[, Nsamp:=NULL] %>%
+  melt(., id.vars=c("year", "month", "fleet")) %>%
+  .[, variable:=as.numeric(gsub("l","",variable))] %>%
+  .[year > 0] %>%
+  setnames(., c("fleet","variable","value"), c("fishery","length","freq")) %>%
+  .[order(fishery, year, month, length)]
+
+# Extract weight comps
+# reformat from wide to long, exclude negative years
+weight.comps <- as.data.table(tmp_data$sizefreq_data_list[[1]]) %>%
+  .[, method:=NULL] %>%
+  .[, sex:=NULL] %>%
+  .[, part:=NULL] %>%
+  .[, Nsamp:=NULL] %>%
+  melt(., id.vars=c("year", "month", "fleet")) %>%
+  .[, variable:=as.numeric(gsub("a","",variable))] %>%
+  .[year > 0] %>%
+  setnames(.,c("fleet","variable","value"), c("fishery","weight","freq")) %>%
+  .[order(fishery, year, month, weight)]
+
+# Write tables
+write.taf(catch, dir="data")
+write.taf(cpue, dir="data")
+write.taf(length.comps, dir="data")
+write.taf(weight.comps, dir="data")
