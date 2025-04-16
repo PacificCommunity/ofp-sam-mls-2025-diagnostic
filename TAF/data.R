3281d633591a58fac3c949728e8bcf5e8f7ac25d
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2025-04-16 11:44:26 +1100

    extract and write out length composition data

--- TAF/data.R
+++ TAF/data.R
@@ -5,9 +5,23 @@
 
 library(TAF)
 library(r4ss)
+library(data.table)
+library(magrittr)
 
 mkdir("data")
 
+# read in Stock Synthesis data file
 tmp_data = SS_readdat("boot/data/data_echo.ss_new")
 
-lencomp <- tmp_data$lencomp
+# extract length composition data; reformat from wide to long
+# only retain observations fitted in the model
+lencomp <- as.data.table(tmp_data$lencomp) %>%
+           .[,part:=NULL] %>%
+           .[,Nsamp:=NULL] %>%
+           melt(.,id.vars=c("year","month","fleet","sex")) %>%
+           .[,variable:=as.numeric(gsub("l","",variable))] %>%
+           .[year>0&value>0] %>%
+           setnames(.,c("fleet","variable","value"),c("fishery","length","freq")) %>%
+           .[order(fishery,year,month,length)]
+
+fwrite(lencomp,"data/lencomp.csv")
