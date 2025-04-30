ff1e7ec68e68534814cb68dc532e29dccc6aab0e
Arni Magnusson <thisisarni@gmail.com>
2025-04-30 11:10:38 +1100

    Add arguments 'measure' and 'zero'

--- TAF/utilities.R
+++ TAF/utilities.R
@@ -1,23 +1,45 @@
-comp2long <- function(x, measure="length")
+comp2long <- function(x, measure=NULL, zero=TRUE)
 {
-  if(!any(names(x) == "method"))
+  # Generalized size comps
+  if(any(names(x) == "method"))
+  {
+    method <- TRUE
+    if(is.null(measure))
+      measure <- "size"
+  }
+  # Length comps
+  else
+  {
+    method <- FALSE
     x <- data.frame(method=1, x)
+    if(is.null(measure))
+      measure <- "length"
+  }
 
-  cols <- c("method", "year", "month", "fleet", "sex", "part")
+  # Store variables as a combined string
+  cols <- c("method", "year", "month", "fleet", "sex", "part", "Nsamp")
+  x$Nsamp <- format(x$Nsamp, trim=TRUE, digits=12)
   rowlab <- apply(x[cols], 1, paste, collapse="|")
 
-  x <- x[grep("[a-z][0-9]+", names(x))]
-  names(x) <- substring(names(x), 2)
+  # Prepare composition data
+  x <- x[!names(x) %in% cols]
+  names(x) <- gsub("[a-z]", "", names(x))
   x <- as.matrix(x)
   row.names(x) <- rowlab
 
-  comps <- as.data.frame(as.table(x), stringsAsFactors=FALSE)
-  vars <- read.table(text=comps$Var1, sep="|")
-  comps$Var1 <- NULL
-  comps$Var2 <- type.convert(comps$Var2, as.is=TRUE)
+  # Convert composition data to long format
+  comp <- as.data.frame(as.table(x), stringsAsFactors=FALSE)
+  vars <- read.table(text=comp$Var1, sep="|", col.names=cols)
+  comp$Var1 <- NULL
+  comp$Var2 <- type.convert(comp$Var2, as.is=TRUE)
+  names(comp) <- c(measure, "freq")
 
-  out <- data.frame(vars, comps)
-  names(out) <- c(cols, measure, "freq")
+  # Combine vars and comp
+  out <- data.frame(vars, comp)
+  if(!method)
+    out$method <- NULL
+  if(!zero)
+    out <- out[out$freq > 0,]
 
   out
 }
