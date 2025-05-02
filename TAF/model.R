8b4eb475f7d18f06f25009c2c101032235456e4f
Arni Magnusson <thisisarni@gmail.com>
2025-05-02 13:41:27 +1100

    Run model

--- TAF/model.R
+++ TAF/model.R
@@ -7,3 +7,16 @@ library(TAF)
 
 mkdir("model")
 
+# Copy executable
+cp("../SS3/ss3_linux",   "model")
+
+# Copy input files
+cp("../SS3/control.ss",  "model")
+cp("../SS3/data.ss",     "model")
+cp("../SS3/forecast.ss", "model")
+cp("../SS3/starter.ss",  "model")
+
+# Run model
+setwd("model")
+system("ss3_linux")
+setwd("..")
