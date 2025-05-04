693ea067a23b139ee68e77b8e78308a2b4031ebb
Arni Magnusson <thisisarni@gmail.com>
2025-05-05 03:08:41 +1100

    Draft code to extract otolith data

--- TAF/data.R
+++ TAF/data.R
@@ -13,6 +13,32 @@ mkdir("data")
 # Read in Stock Synthesis data file
 dat <- SS_readdat("boot/data/data_echo.ss_new")
 
+# Otoliths
+oto <- dat$agecomp
+x <- oto
+x$Nsamp <- NULL
+cols <- c("year", "month", "fleet", "sex", "part", "ageerr", "Lbin_lo",
+          "Lbin_hi")
+# Store variables as a combined string
+rowlab <- apply(x[cols], 1, paste, collapse = "|")
+# Prepare composition data
+x <- x[!names(x) %in% cols]
+names(x) <- gsub("[a-z]", "", names(x))
+x <- as.matrix(x)
+row.names(x) <- rowlab
+# Convert composition data to long format
+comp <- as.data.frame(as.table(x), stringsAsFactors = FALSE)
+vars <- read.table(text = comp[["Var1"]], sep = "|", col.names = cols)
+comp[["Var1"]] <- NULL
+comp[["Var2"]] <- type.convert(comp[["Var2"]], as.is = TRUE)
+names(comp) <- c("age", "freq")
+# Combine vars and comp
+out <- data.frame(vars, comp)
+# Remove zero freq
+out <- out[out$freq > 0,]
+# One line per sample
+out <- out[rep(seq_len(nrow(out)), out$freq),]
+
 # Extract catch
 catch <- dat$catch
 names(catch) <- c("year", "season", "fishery", "catch", "cv")
